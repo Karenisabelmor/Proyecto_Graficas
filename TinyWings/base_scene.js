516ac113eddd72a1ca67e43191a86ceb02c70ddd
Ana Pao Minchaca <42215143+anapao-minchaca@users.noreply.github.com>
2022-05-20 14:39:49 -0500

    avance de funcionalidades

--- TinyWings/base_scene.js
+++ TinyWings/base_scene.js
@@ -7,8 +7,9 @@ import { FBXLoader } from '../libs/three.js/loaders/FBXLoader.js';
 
 // checar ejemplo 11_threejsInteraction
 
-let renderer = null, scene = null, camera = null, group = null, objectList = [], orbitControls = null;
+let renderer = null, scene = null, camera = null, groupHill = new THREE.Object3D, objectList = [], orbitControls = null;
 let directionalLight = null, hemisphereLight = null;
+let paused = false;
 
 let objModelUrl = {obj:'./models/apple/Apple.obj', map:'./models/apple/Apple_BaseColor.png', normalMap:'./models/apple/Apple_Normal.png', specularMap: './models/apple/Apple_Roughtness.png'};
 
@@ -23,6 +24,23 @@ function getConfig() {
     } else {
         return JSON.parse(window.localStorage.getItem('configuracion'));
     }
+}
+
+function animate(){
+
+    groupHill.position.x = groupHill.position.x + 0.1;
+    groupHill.position.z = groupHill.position.z - 0.02;
+}
+
+function update() 
+{
+    requestAnimationFrame(function() { update(); });
+
+    renderer.render( scene, camera );
+
+    orbitControls.update();
+    
+    animate();
 }
 
 function main()
@@ -40,12 +58,13 @@ function main()
 const fbxLoader = new FBXLoader()
 fbxLoader.load(
     './models/cloud/Cloud_1.fbx',
-    (object) => {
-        object.scale.set(.02, .02, .02)
-        object.position.z = -7;
-        object.position.x = -16;
-        object.position.y = 6;
-        scene.add(object)
+    (cloud) => {
+        cloud.scale.set(.02, .02, .02)
+        cloud.position.z = -7;
+        cloud.position.x = -16;
+        cloud.position.y = 6;
+        scene.add(cloud);
+        groupHill.add(cloud);
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -58,13 +77,14 @@ fbxLoader.load(
 const fbxLoader_Cerdo = new FBXLoader()
 fbxLoader_Cerdo.load(
     './models/cerdo/cerdito_alas.fbx',
-    (object) => {
-        object.scale.set(0.025, 0.025, 0.025)
-        object.position.z = -11;
-        object.position.x = 5;
-        object.rotation.y = 5;
-        object.position.y = 3;
-        scene.add(object)
+    (cerdito) => {
+        cerdito.scale.set(0.025, 0.025, 0.025)
+        cerdito.position.z = -11;
+        cerdito.position.x = 5;
+        cerdito.rotation.y = 5;
+        cerdito.position.y = 3;
+        scene.add(cerdito)
+        cerdito.name = "cerdito"
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -77,13 +97,14 @@ fbxLoader_Cerdo.load(
 const fbxLoader_Hills = new FBXLoader()
 fbxLoader_Hills.load(
     './models/hills/hills.fbx',
-    (object) => {
-        object.scale.set(0.050, 0.050, 0.050)
-        object.position.z = 30;
-        object.position.x = -220;
-        object.position.y = -3;
-        object.rotation.y = 30;
-        scene.add(object)
+    (hill) => {
+        hill.scale.set(0.050, 0.050, 0.050)
+        hill.position.z = 30;
+        hill.position.x = -220;
+        hill.position.y = -3;
+        hill.rotation.y = 30;
+        scene.add(hill);
+        groupHill.add(hill);
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -96,13 +117,14 @@ fbxLoader_Hills.load(
 const fbxLoader_Enemies = new FBXLoader()
 fbxLoader_Enemies.load(
     './models/enemies/pigeon.fbx',
-    (object) => {
-        object.scale.set(0.0050, 0.0050, 0.0050)
-        object.position.z = -7;
-        object.position.x = -16;
-        object.rotation.y = 1.7;
-        object.position.y = 6;
-        scene.add(object)
+    (pigeon) => {
+        pigeon.scale.set(0.0050, 0.0050, 0.0050)
+        pigeon.position.z = -7;
+        pigeon.position.x = -16;
+        pigeon.rotation.y = 1.7;
+        pigeon.position.y = 6;
+        scene.add(pigeon);
+        groupHill.add(pigeon);
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -115,13 +137,14 @@ fbxLoader_Enemies.load(
 const fbxLoader_Pup1 = new FBXLoader()
 fbxLoader_Pup1.load(
     './models/power_ups/pwr1.fbx',
-    (object) => {
-        object.scale.set(0.0050, 0.0050, 0.0050)
-        object.position.z = -11;
-        object.position.x = -10;
-        object.rotation.y = 2;
-        object.position.y = 6;
-        scene.add(object)
+    (pw1) => {
+        pw1.scale.set(0.0050, 0.0050, 0.0050)
+        pw1.position.z = -11;
+        pw1.position.x = -10;
+        pw1.rotation.y = 2;
+        pw1.position.y = 6;
+        scene.add(pw1);
+        groupHill.add(pw1);
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -134,13 +157,14 @@ fbxLoader_Pup1.load(
 const fbxLoader_Pup2 = new FBXLoader()
 fbxLoader_Pup2.load(
     './models/power_ups/pwr2.fbx',
-    (object) => {
-        object.scale.set(0.0050, 0.0050, 0.0050)
-        object.position.z = -7;
-        object.position.x = -10;
-        object.rotation.y = 2;
-        object.position.y = 6;
-        scene.add(object)
+    (pw2) => {
+        pw2.scale.set(0.0050, 0.0050, 0.0050)
+        pw2.position.z = -7;
+        pw2.position.x = -10;
+        pw2.rotation.y = 2;
+        pw2.position.y = 6;
+        scene.add(pw2);
+        groupHill.add(pw2);
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -153,13 +177,14 @@ fbxLoader_Pup2.load(
 const fbxLoader_Pup3 = new FBXLoader()
 fbxLoader_Pup3.load(
     './models/power_ups/pwr3.fbx',
-    (object) => {
-        object.scale.set(0.0050, 0.0050, 0.0050)
-        object.position.z = -13;
-        object.position.x = -13;
-        object.rotation.y = 2;
-        object.position.y = 6;
-        scene.add(object)
+    (pw3) => {
+        pw3.scale.set(0.0050, 0.0050, 0.0050)
+        pw3.position.z = -13;
+        pw3.position.x = -13;
+        pw3.rotation.y = 2;
+        pw3.position.y = 6;
+        scene.add(pw3);
+        groupHill.add(pw3);
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
@@ -197,6 +222,7 @@ async function loadObj(objModelUrl, objectList)
         
         objectList.push(object);
         scene.add(object);
+        groupHill.add(object);
     }
     catch (err) 
     {
@@ -204,15 +230,32 @@ async function loadObj(objModelUrl, objectList)
     }
 }
 
-
-function update() 
-{
-    requestAnimationFrame(function() { update(); });
-
-    renderer.render( scene, camera );
-
-    orbitControls.update();
-}
+function onKeyDown ( event ) {
+    var cerdo = scene.getObjectByName('cerdito');
+    console.log(cerdo);
+    switch ( event.keyCode ) {
+        case 37: // left
+        case 65: // a
+            // cerdo.rotation.x += 0.1;
+            cerdo.position.z += 0.5;
+            break;
+        case 38: // up
+        case 87: // w
+            //cerdo.rotation.z -= 0.1;
+            cerdo.position.y += 0.5;
+            break;
+        case 39: // right
+        case 68: // d
+            //cerdo.rotation.x -= 0.1;
+            cerdo.position.z -= 0.5;
+            break;
+        case 40: // down
+        case 83: // s
+            //cerdo.rotation.z += 0.1;
+            cerdo.position.y -= 0.5;
+            break;
+    }
+};
 
 function createScene(canvas) 
 {
@@ -235,6 +278,9 @@ function createScene(canvas)
     // camera.position.set(20, 10, -30);
     camera.position.set(20, 10, -25);
 
+    // events
+    document.addEventListener( 'keydown', onKeyDown, false );
+
     // Orbit controls
     orbitControls = new OrbitControls(camera, renderer.domElement);
 
@@ -248,8 +294,18 @@ function createScene(canvas)
     hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9);
     scene.add(hemisphereLight);
 
+    // Audio
+    window.addEventListener("DOMContentLoaded", event => {
+        const audio = document.querySelector("audio");
+        audio.volume = 0.2;
+        audio.play();
+    });
+
     // Create the objects
     loadObj(objModelUrl, objectList);
+
+    // Group objects
+    scene.add(groupHill)
 }
 
 function resize()
